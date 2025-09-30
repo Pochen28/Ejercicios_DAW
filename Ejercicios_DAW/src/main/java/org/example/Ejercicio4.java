bf03b51a7ce2bd96e2a46de401053153d570ec46
A611\1DAW <rieraljoseluis202@gmail.com>
2025-09-30 21:18:09 +0200

    Queda por terminar el 4

--- Ejercicios_DAW/src/main/java/org/example/Ejercicio4.java
+++ Ejercicios_DAW/src/main/java/org/example/Ejercicio4.java
@@ -6,6 +6,14 @@ public class Ejercicio4 {
     static void main() {
         Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Introduce el primer número: ");
+        int numero1 = teclado.nextInt();
+
+        System.out.println("Introduce el segundo número: ");
+        int numero2 = teclado.nextInt();
+
+        System.out.println("Introduce el tercer número: ");
+        int numero3 = teclado.nextInt();
 
     }
 }
