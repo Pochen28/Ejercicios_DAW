b989af84341c9e691db82da2d03ef051040d1778
A611\1DAW <rieraljoseluis202@gmail.com>
2025-10-17 17:15:19 +0200

    EjercicioGeneraciones y continuación práctica.

--- Ejercicios_DAW/src/main/java/org/example/Practica_5.java
+++ Ejercicios_DAW/src/main/java/org/example/Practica_5.java
@@ -11,12 +11,26 @@ public class Practica_5 {
 
     System.out.println("Introduce tu fecha de nacimiento dd/mm/aaaa: ");
     String nacimiento_palabra = teclado.nextLine();
-    nacimiento_palabra = nacimiento_palabra.replace("/", " ");
+    nacimiento_palabra = nacimiento_palabra.replace("/", "").replace("-" , "");
     System.out.println(nacimiento_palabra);
+    int dia = Integer.parseInt(nacimiento_palabra.substring(0,2));
+    int mes = Integer.parseInt(nacimiento_palabra.substring(2,4));
+    int anyo = Integer.parseInt(nacimiento_palabra.substring(4,8));
+    System.out.println(dia);
+    System.out.println(mes);
+    System.out.println(anyo);
+
 
     do {
+        try {
 
-    }while ()
+
+
+        }catch (Exception Err) {
+
+
+        }
+    }while (repetir==true);
 
 
 
