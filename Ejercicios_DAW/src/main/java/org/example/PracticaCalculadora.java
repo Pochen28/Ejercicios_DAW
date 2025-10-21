a4222ce72400601275008efbff046aa3163047aa
A611\1DAW <rieraljoseluis202@gmail.com>
2025-10-21 21:10:31 +0200

    Continuación práctica Calculadora

--- Ejercicios_DAW/src/main/java/org/example/PracticaCalculadora.java
+++ Ejercicios_DAW/src/main/java/org/example/PracticaCalculadora.java
@@ -5,20 +5,35 @@ import java.util.Scanner;
 public class PracticaCalculadora {
     static void main() {
         Scanner teclado = new Scanner(System.in);
-        double num = teclado.nextDouble();
+
 
         System.out.println("BIENVENIDO");
 
-        switch (num){
-            case 1:
-                System.out.println("a");
-            case 2:
-                System.out.println();
-            case 3:
-            case 4:
-            case 5:
-            case 6:
 
+
+
+        System.out.println("Introduce un número:");
+        int num1 = teclado.nextInt();
+        System.out.println("Elige una operación: ");
+        System.out.println(" 1.(+) \n 2.(-) \n 3.(x) \n 4.(/) \n 5.(R) ");
+        String signo = teclado.next();
+        System.out.println("Introduce otro número para poder sumarlo con el primero:");
+        double num2 = teclado.nextDouble();
+
+
+        switch ( signo){
+            case "+":
+                System.out.println("Comencemos a sumar.");
+                double suma = num1 + num2;
+                System.out.println(suma);
+
+
+            case "-":
+                System.out.println("Comencemos a restar.");
+                double resta = num1 - num2;
+                System.out.println(resta);
+            case "/":
+                System.out.println( "" );
 
         }
 
