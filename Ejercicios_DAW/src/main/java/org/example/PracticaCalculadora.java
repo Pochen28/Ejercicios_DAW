3ba2f0560f108e3857a9cfcb19ab751c4b52edb0
DESKTOP-DUKR00O\Usuario <rieraljoseluis202@gmail.com>
2025-10-23 02:55:20 +0200

    Práctica Calculadora terminada.

--- Ejercicios_DAW/src/main/java/org/example/PracticaCalculadora.java
+++ Ejercicios_DAW/src/main/java/org/example/PracticaCalculadora.java
@@ -1,102 +1,120 @@
 package org.example;
 
-import javax.xml.stream.util.XMLEventAllocator;
 import java.util.Scanner;
+
 
 public class PracticaCalculadora {
     static void main() {
         Scanner teclado = new Scanner(System.in);
         boolean repetir = true;
         boolean repetir2 = true;
+        boolean repetir3 = true;
         double num1 = 0;
         double num2 = 0;
-        System.out.println("BIENVENIDO");
+        System.out.println("BIENVENIDO A CALCULATOR");
         do {
             System.out.println("Introduce un número:");
 
-
-            if (teclado.hasNextDouble()){
+            if (teclado.hasNextDouble()) {
                 num1 = teclado.nextDouble();
                 repetir = true;
-            }else {
+            } else {
                 System.out.println("ERROR, sólo se aceptan números.");
                 repetir = false;
                 teclado.nextLine();
             }
 
-        }while (repetir == false);
-
-        System.out.println("Elige una operación: ");
-        System.out.println(" 1.(+) \n 2.(-) \n 3.(x) \n 4.(/) \n 5.(R) ");
-        String signo = teclado.next();
-
-        String signo_mayus = signo.toUpperCase();
-        boolean iguales = signo_mayus.equals("R");
-
-        if (signo.length()== 1);
-             System.out.println("hola");
+        } while (repetir == false);
 
 
-        if (iguales){
-            if (num1 > 0){
-                System.out.println("Haremos la raíz del número");
-                double raiz = Math.sqrt(num1);
-                System.out.println(raiz);
-            }else {
-                System.out.println("ERROR, no existen las raíces cuadradas para los números negativos.");
-            }
+        do {
+            System.out.println("Elige una operación: ");
+            System.out.println(" 1.(+) \n 2.(-) \n 3.(x) \n 4.(/) \n 5.(R) ");
+            System.out.println("--------");
+            String signo = teclado.next();
+            String signo_mayus = signo.toUpperCase();
+            char verificar = signo.charAt(0);
+            if (verificar == '+' || verificar == '-' || verificar == '/' || verificar == 'x' || verificar == 'R') {
+                if (signo.length() == 1) {
 
-        }else {
-            do {
-                System.out.println("Introduce otro número para poder operarlo con el primero:");
-                if (teclado.hasNextDouble()){
-                    num2 = teclado.nextDouble();
-                    repetir2 = true;
-                }else {
-                    System.out.println("Error, sólo se aceptan números.");
-                    repetir2 = false;
+                    boolean iguales = signo_mayus.equals("R");
+                    repetir3 = true;
+                    if (iguales) {
+                        if (num1 > 0) {
+                            System.out.println("Haremos la raíz del número");
+                            double raiz = Math.sqrt(num1);
+                            System.out.println("RESULTADO = " + raiz);
+                        } else {
+                            System.out.println("ERROR, no existen las raíces cuadradas para los números negativos.");
+                        }
+
+                    } else {
+                        do {
+                            System.out.println("Introduce otro número para poder operarlo con el primero:");
+                            if (teclado.hasNextDouble()) {
+                                num2 = teclado.nextDouble();
+                                repetir2 = true;
+                            } else {
+                                System.out.println("Error, sólo se aceptan números.");
+                                repetir2 = false;
+                                teclado.nextLine();
+                            }
+                        } while (repetir2 = false);
+
+
+                        switch (signo) {
+                            case "+":
+                                System.out.println("Comencemos a sumar.");
+                                double suma = num1 + num2;
+                                System.out.println("RESULTADO = " + suma);
+                                break;
+
+                            case "-":
+                                System.out.println("Comencemos a restar.");
+                                double resta = num1 - num2;
+                                System.out.println("RESULTADO = " + resta);
+                                break;
+
+                            case "x":
+                                System.out.println("Comencmos a multiplicar.");
+                                double multiplicacion = num1 * num2;
+                                System.out.println("RESULTADO = " + multiplicacion);
+                                break;
+
+                            case "/":
+                                System.out.println("Comencemos a dividir. ");
+
+                                if (num2 == 0) {
+                                    System.out.println("Error, no se pueden dividir números entre cero.");
+                                } else {
+                                    double division = num1 / num2;
+                                    System.out.println("RESULTADO = " + division);
+                                    break;
+                                }
+
+
+                        }
+                    }
+                } else {
+                    System.out.println("ERROR, debes introducir el signo para llevar a cabo la operación.");
+                    repetir3 = false;
                     teclado.nextLine();
                 }
-            }while (repetir2 = false);
 
 
-
-            switch ( signo){
-                case "+":
-                    System.out.println("Comencemos a sumar.");
-                    double suma = num1 + num2;
-                    System.out.println(suma);
-                    break;
-
-
-                case "-":
-                    System.out.println("Comencemos a restar.");
-                    double resta = num1 - num2;
-                    System.out.println(resta);
-                    break;
-
-                case "x":
-                    System.out.println("Comencmos a multiplicar.");
-                    double multiplicacion = num1 * num2;
-                    System.out.println(multiplicacion);
-                    break;
-                case "/":
-                    System.out.println("Comencemos a dividir. ");
-
-                    if (num2 == 0){
-                        System.out.println("Error, no se pueden dividir números entre cero.");
-                    }else {
-                        double division = num1 / num2;
-                        System.out.println(division);
-                        break;
-                    }
-
-
-
-
+            } else {
+                System.out.println("ERROR, debes introducir únicamente los símbolos indicados.");
+                repetir3 = false;
+                teclado.nextLine();
             }
-        }
+
+        } while (repetir3 == false);
     }
 }
+
+
+
+
+
 
 
--- Ejercicios_DAW/src/main/java/org/example/String_Cosas.java
+++ Ejercicios_DAW/src/main/java/org/example/String_Cosas.java
@@ -5,48 +5,75 @@ import java.util.Scanner;
 public class String_Cosas {
     static void main() {
         Scanner teclado = new Scanner(System.in);
-        String frase = "Me siento empodera -a-a, sigo empodera";
 
-        int tamanyo = frase.length();
-        System.out.println(tamanyo);
+        String signo = teclado.next();
 
-        System.out.println(frase.charAt(15));
+        switch (signo) {
+            case "+":
+                System.out.println("Comencemos a sumar.");
 
-        String trozo = frase.substring(10,22);
-        System.out.println(trozo);
-        String trozo2  = frase.substring(22);
-        System.out.println(trozo2);
+                break;
 
-        int posicion_empodera = frase.indexOf("empodera");
-        System.out.println(posicion_empodera);
 
-        int posicion_empodera2 = frase.indexOf("empodera", 1);
-        System.out.println(posicion_empodera2);
+            case "-":
+                System.out.println("Comencemos a restar.");
 
-        int posicion_empodera_ult = frase.lastIndexOf("empodera");
-        System.out.println(posicion_empodera_ult);
+                break;
 
-        String frase_mayusculas = frase.toUpperCase();
-        System.out.println(frase_mayusculas);
+            case "x":
+                System.out.println("Comencmos a multiplicar.");
 
-        String frase_minusculas = frase.toLowerCase();
-        System.out.println(frase_minusculas);
+                break;
+            case "/":
+                System.out.println("Comencemos a dividir. ");
 
-        String nombre = "Iván                     ";
-        System.out.println(nombre.trim() + " - " + nombre + " #");
-        String nombre_mayus = nombre.toUpperCase();
-        boolean iguales = nombre_mayus.trim().equals("IVÁN");
-        System.out.println(iguales);
 
-        boolean iguales2 = nombre.trim().equalsIgnoreCase("Iván");
-        System.out.println(iguales2);
+                String frase = "Me siento empodera -a-a, sigo empodera";
 
-        String cambiada = frase.replace("empodera", "patriota");
-        System.out.println(cambiada);
+                int tamanyo = frase.length();
+                System.out.println(tamanyo);
 
-        String frase_concatenada = frase.concat(" Misha dame un becico");
-        System.out.println(frase_concatenada);
+                System.out.println(frase.charAt(15));
+
+                String trozo = frase.substring(10, 22);
+                System.out.println(trozo);
+                String trozo2 = frase.substring(22);
+                System.out.println(trozo2);
+
+                int posicion_empodera = frase.indexOf("empodera");
+                System.out.println(posicion_empodera);
+
+                int posicion_empodera2 = frase.indexOf("empodera", 1);
+                System.out.println(posicion_empodera2);
+
+                int posicion_empodera_ult = frase.lastIndexOf("empodera");
+                System.out.println(posicion_empodera_ult);
+
+                String frase_mayusculas = frase.toUpperCase();
+                System.out.println(frase_mayusculas);
+
+                String frase_minusculas = frase.toLowerCase();
+                System.out.println(frase_minusculas);
+
+                String nombre = "Iván                     ";
+                System.out.println(nombre.trim() + " - " + nombre + " #");
+                String nombre_mayus = nombre.toUpperCase();
+                boolean iguales = nombre_mayus.trim().equals("IVÁN");
+                System.out.println(iguales);
+
+                boolean iguales2 = nombre.trim().equalsIgnoreCase("Iván");
+                System.out.println(iguales2);
+
+                String cambiada = frase.replace("empodera", "patriota");
+                System.out.println(cambiada);
+
+                String frase_concatenada = frase.concat(" Misha dame un becico");
+                System.out.println(frase_concatenada);
+
+            default:
+                System.out.println("ninguno mierdas");
+
+        }
 
     }
-
 }
