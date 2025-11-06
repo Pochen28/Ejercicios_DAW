eef95a753440acd4860940520b700ba9f955ae5a
A611\1DAW <rieraljoseluis202@gmail.com>
2025-11-06 21:17:00 +0100

    Cont Arrays

---
+++ Ejercicios_DAW/src/main/java/org/example/Ej4_Vectores.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.lang.foreign.SymbolLookup;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ej4_Vectores {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
+
+        int vector [] = {3,4,5,4,9};
+
+        int nuevo[] = new int [vector.length-1];
+
+        int posicion_borrar = 3;
+
+        for (int i = 0; i < nuevo.length ; i ++) {
+
+            if (i>= posicion_borrar) {
+                nuevo [i] = vector [i + 1];
+
+            }else {
+                nuevo [i] = vector [i];
+
+            }
+
+        }
+        System.out.println(Arrays.toString(nuevo));
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Ejercicios_DAW/src/main/java/org/example/Ej5_Vectores.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.util.Arrays;
+
+public class Ej5_Vectores {
+    static void main() {
+
+        int [] vector = {1,2,3,4,5};
+
+        int numAux = vector[0];
+
+        for (int i = 0; i < vector.length -1; i++) {
+            vector[i] = vector [i + 1];
+        }
+
+        vector [vector.length -1] = numAux;
+
+        System.out.println(Arrays.toString(vector));
+    }
+}
