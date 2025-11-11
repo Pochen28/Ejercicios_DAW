ff59090581f0066999ac06ac53a9014d2e87e944
DESKTOP-DUKR00O\Usuario <rieraljoseluis202@gmail.com>
2025-11-11 14:50:06 +0100

    Samurais terminada

--- Ejercicios_DAW/src/main/java/org/example/PracticaCalculadora.java
+++ Ejercicios_DAW/src/main/java/org/example/PracticaCalculadora.java
@@ -32,12 +32,10 @@ public class PracticaCalculadora {
         do {
             System.out.println("Elige una operación: ");
 
-<<<<<<< HEAD
+
             System.out.println(" 1.(+) \n 2.(-) \n 3.(X) \n 4.(/) \n 5.(R) ");
 
             System.out.println(" 1.(+) \n 2.(-) \n 3.(x) \n 4.(/) \n 5.(R) ");
-=======
-
 
 
             System.out.println(" 1.(+) \n 2.(-) \n 3.(x) \n 4.(/) \n 5.(R) ");
@@ -46,120 +44,97 @@ public class PracticaCalculadora {
             System.out.println(" 1.(+) \n 2.(-) \n 3.(X) \n 4.(/) \n 5.(R) ");
 
 
-
->>>>>>> ee66348105c6e504d4d20d21db7121041c8dcac9
             System.out.println("--------");
             String signo = teclado.next();
             String signo_mayus = signo.toUpperCase();
             char verificar = signo_mayus.charAt(0);
-<<<<<<< HEAD
+
 
             if (verificar == '+' || verificar == '-' || verificar == '/' || verificar == 'X' || verificar == 'R') { // Comprueba que el símbolo esté entre los permitidos
 
-                if (verificar == '+' || verificar == '-' || verificar == '/' || verificar == 'x' || verificar == 'R') { // Comprueba que el símbolo esté entre los permitidos
-                    if (signo_mayus.length() == 1) {
+                if (signo_mayus.length() == 1) {
 
-=======
-
-
-
-
-
-
-
-
-                if (verificar == '+' || verificar == '-' || verificar == '/' || verificar == 'X' || verificar == 'R') { // Comprueba que el símbolo esté entre los permitidos
-
-                    if (signo_mayus.length() == 1) {
-
->>>>>>> ee66348105c6e504d4d20d21db7121041c8dcac9
-                        boolean iguales = signo_mayus.equals("R");
-                        repetir3 = true;
-                        if (iguales) {
-                            if (num1 > 0) { // Control para no hacer raíz cuadrada de negativos
-                                System.out.println("Haremos la raíz del número");
-                                double raiz = Math.sqrt(num1);
-                                System.out.println("RESULTADO = " + raiz);
-                            } else {
-                                System.out.println("ERROR, no existen las raíces cuadradas para los números negativos.");
-                            }
-
+                    boolean iguales = signo_mayus.equals("R");
+                    repetir3 = true;
+                    if (iguales) {
+                        if (num1 > 0) { // Control para no hacer raíz cuadrada de negativos
+                            System.out.println("Haremos la raíz del número");
+                            double raiz = Math.sqrt(num1);
+                            System.out.println("RESULTADO = " + raiz);
                         } else {
-                            // Si no es raíz, pido el segundo número
-                            do {
-                                System.out.println("Introduce otro número para poder operarlo con el primero:");
-                                if (teclado.hasNextDouble()) {
-                                    num2 = teclado.nextDouble();
-                                    repetir2 = true;
-                                } else {
-                                    System.out.println("Error, sólo se aceptan números.");
-                                    repetir2 = false;
-                                    teclado.nextLine();
-                                }
-                            } while (repetir2 = false);
-
-
-                            switch (signo_mayus) { // Introduzco un switch para realizar la operación seleccionada
-                                case "+":
-                                    System.out.println("Comencemos a sumar.");
-                                    double suma = num1 + num2;
-                                    System.out.println("RESULTADO = " + suma);
-                                    break;
-
-                                case "-":
-                                    System.out.println("Comencemos a restar.");
-                                    double resta = num1 - num2;
-                                    System.out.println("RESULTADO = " + resta);
-                                    break;
-
-                                case "x":
-                                    System.out.println("Comencemos a multiplicar.");
-                                    double multiplicacion = num1 * num2;
-                                    System.out.println("RESULTADO = " + multiplicacion);
-                                    break;
-
-                                case "/":
-
-
-                                    if (num2 == 0) { // Control para evitar la división entre cero
-                                        System.out.println("Error, no se pueden dividir números entre cero.");
-                                    } else {
-                                        System.out.println("Comencemos a dividir. ");
-                                        double division = num1 / num2;
-                                        System.out.println("RESULTADO = " + division);
-                                        break;
-                                    }
-
-
-                            }
+                            System.out.println("ERROR, no existen las raíces cuadradas para los números negativos.");
                         }
+
                     } else {
-                        System.out.println("ERROR, debes introducir el signo para llevar a cabo la operación.");
-                        repetir3 = false;
-                        teclado.nextLine();
+                        // Si no es raíz, pido el segundo número
+                        do {
+                            System.out.println("Introduce otro número para poder operarlo con el primero:");
+                            if (teclado.hasNextDouble()) {
+                                num2 = teclado.nextDouble();
+                                repetir2 = true;
+                            } else {
+                                System.out.println("Error, sólo se aceptan números.");
+                                repetir2 = false;
+                                teclado.nextLine();
+                            }
+                        } while (repetir2 = false);
+
+
+                        switch (signo_mayus) { // Introduzco un switch para realizar la operación seleccionada
+                            case "+":
+                                System.out.println("Comencemos a sumar.");
+                                double suma = num1 + num2;
+                                System.out.println("RESULTADO = " + suma);
+                                break;
+
+                            case "-":
+                                System.out.println("Comencemos a restar.");
+                                double resta = num1 - num2;
+                                System.out.println("RESULTADO = " + resta);
+                                break;
+
+                            case "x":
+                                System.out.println("Comencemos a multiplicar.");
+                                double multiplicacion = num1 * num2;
+                                System.out.println("RESULTADO = " + multiplicacion);
+                                break;
+
+                            case "/":
+
+
+                                if (num2 == 0) { // Control para evitar la división entre cero
+                                    System.out.println("Error, no se pueden dividir números entre cero.");
+                                } else {
+                                    System.out.println("Comencemos a dividir. ");
+                                    double division = num1 / num2;
+                                    System.out.println("RESULTADO = " + division);
+                                    break;
+                                }
+
+
+                        }
                     }
-
-
                 } else {
-                    System.out.println("ERROR, debes introducir únicamente los símbolos indicados.");
+                    System.out.println("ERROR, debes introducir el signo para llevar a cabo la operación.");
                     repetir3 = false;
                     teclado.nextLine();
                 }
 
-<<<<<<< HEAD
+
+            } else {
+                System.out.println("ERROR, debes introducir únicamente los símbolos indicados.");
+                repetir3 = false;
+                teclado.nextLine();
+
             }
 
-        } while (repetir3 == false) ; // Repite mientras haya un error con el signo
+        }while (repetir3 == false);
     }
-
-}
-=======
-
-
-        } while (repetir3 = false);
-
-    }
-
 }
 
->>>>>>> ee66348105c6e504d4d20d21db7121041c8dcac9
+
+
+
+
+
+
