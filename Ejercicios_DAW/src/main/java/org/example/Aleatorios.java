e30aae190af14d8983f37d3a6334a24d4895141b
A611\1DAW <rieraljoseluis202@gmail.com>
2025-11-26 21:19:49 +0100

    Bingo

--- Ejercicios_DAW/src/main/java/org/example/Aleatorios.java
+++ Ejercicios_DAW/src/main/java/org/example/Aleatorios.java
@@ -6,9 +6,15 @@ public class Aleatorios {
     public static void main(String[] args) {
         Random aleatorio = new Random();
         String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
+        int num = 0;
         int longitud = 12;
         String contrasena = "";
+
+        for (int i = 0; i < 10; i++) {
+            num = aleatorio.nextInt(4,8)+1;
+            System.out.print(num + " ");
+        }
+
 
         for (int i = 0; i < longitud; i++) {
             int coger = aleatorio.nextInt(caracteres.length());
---
+++ Ejercicios_DAW/src/main/java/org/example/EjercicoBingo.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class EjercicoBingo {
+    static void main(String [] args) {
+        Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
+
+        int carton [] [] = new int [3] [3];
+
+        for (int i = 0; i < carton.length; i++) {
+            for (int j = 0; j < carton [i].length; j++) {
+                carton [i] [j] = aleatorio.nextInt(0,90)+1;
+            }
+        }
+        for (int filas [] : carton){
+            for (int columnas  : filas){
+                System.out.print(columnas + "");
+            }
+            System.out.print("\n");
+        }
+    }
+}
