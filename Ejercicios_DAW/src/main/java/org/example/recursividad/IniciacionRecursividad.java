7837c0bb5a34eb1695840b5b1ea09433b30cfadf
A611\1DAW <rieraljoseluis202@gmail.com>
2026-01-08 21:20:01 +0100

    no message

---
+++ Ejercicios_DAW/src/main/java/org/example/recursividad/IniciacionRecursividad.java
@@ -0,0 +1,19 @@
+package org.example.recursividad;
+
+public class IniciacionRecursividad {
+    static int limite = 10;
+    static void main() {
+        imprimir (1);
+    }
+
+    public static void imprimir (int num){
+
+
+        if (num<= limite){
+            return;
+        }else {
+            System.out.println(num);
+            imprimir(num+1);
+        }
+    }
+}
